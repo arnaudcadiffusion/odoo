7779ee6924372b8d428896df09b85cc2a0126f98
roger <roger@nexources.com>
2026-04-16 16:00:06 +0300

    [ADD]: ca_date_format

---
+++ addons_project/ca_date_format/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "CA Date Format",
+    "version": "19.0.1.0.0",
+    "summary": "Display of the date in dd/mm/yyyy format",
+    "category": "Custom",
+    "author": "Nexources",
+    "depends": [
+        "purchase",
+        "cadiffusion_base",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
