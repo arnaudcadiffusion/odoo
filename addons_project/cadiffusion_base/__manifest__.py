5032d8f1990327c34cb8d2d520c0352dda911a34
MaximilienNaina <maximilien@nexources.com>
2026-06-23 14:49:03 +0300

    [MERGE]

--- addons_project/cadiffusion_base/__manifest__.py
+++ addons_project/cadiffusion_base/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'CaDiffusion Base',
-    'version': '19.0.1.0.10',
+    'version': '19.0.1.0.11',
     'summary': 'Champs et vues personnalisés Cadiffusion (migration depuis Studio)',
     'category': 'Custom',
     'author': 'Cadiffusion',
